97252cd52aee51ee59f8f17808269e94d0c6c11b
David Whyman <whyman@uchicago.edu>
2018-04-11 14:52:24 -0400

    relational

---
+++ Class 6_Apr 11_Week 3_Wed.R
@@ -0,0 +1,7 @@
+#Relational Data
+#not just one table, combination, relations between multiple data frames encode information
+#Relations are between two tables only, can relate a->b then ab->c but not a->b<-c
+
+#relational verbs:
+#mutating joins: create new columns in table
+#filter values in table
